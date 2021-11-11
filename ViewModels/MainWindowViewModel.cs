952b1bded64377b5319d627a62bdd131b39d7b38
Alexandr Nebajkin <Alexander.Nebajkin@esterdev.com>
2021-11-11 22:25:59 +0300

    Init.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PoloniexBot.ViewModels.Base;
+
+namespace PoloniexBot.ViewModels
+{
+	internal class MainWindowViewModel : ViewModel
+	{
+		private string title = "Poloniex Bot";
+
+		/// <summary>
+		/// Заголовок окна.
+		/// </summary>
+		public string Title
+		{
+			get => this.title;
+			set => this.Set(ref this.title, value);
+		}
+	}
+}
