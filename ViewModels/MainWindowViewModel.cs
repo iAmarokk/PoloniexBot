97c18dee01b527aaaa96201681363cab49d928dc
Alexandr Nebajkin <Alexander.Nebajkin@esterdev.com>
2021-11-13 22:52:40 +0300

    Edit MainForm.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using PoloniexBot.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -28,13 +29,25 @@ namespace PoloniexBot.ViewModels
 			set => this.Set(ref this.selectedCoin, value);
 		}
 
+		public decimal PriceBTC { get; set; }
+		public decimal TotalAssets { get; set; }
+
 		public ObservableCollection<Coin> Currencies { get; set; }
 
 		public MainWindowViewModel()
 		{
-			decimal priceBTC = BTCPrice.Get();
+			this.PriceBTC = BTCPrice.Get();
 			CompleteBalances balances = new CompleteBalances();
 			this.Currencies = balances.Get();
+			foreach(Coin item in this.Currencies)
+			{
+				decimal value = decimal.Parse(item.Values.btcValue, CultureInfo.InvariantCulture);
+
+				if (value > 0)
+				{
+					this.TotalAssets += value * this.PriceBTC;
+				}
+			}
 		}
 	}
 }
